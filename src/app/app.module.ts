ad22f24f5e5c9f6c2c3edeb5402e4ce7e29ae21e
yasharsanayee <yasharsanayee@gmail.com>
2023-03-04 11:08:48 +0330

    leaflet package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataStoreModule } from './data-store/data-store.module';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, DataStoreModule],
+  imports: [BrowserModule, AppRoutingModule, DataStoreModule, LeafletModule],
   providers: [],
   bootstrap: [AppComponent],
 })
