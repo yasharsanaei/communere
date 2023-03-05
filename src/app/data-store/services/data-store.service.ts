dff24f5e188d2b47c021174c299fe5b084749b97
yasharsanayee <yasharsanayee@gmail.com>
2023-03-05 16:13:27 +0330

    refactored data service

--- src/app/data-store/services/data-store.service.ts
+++ src/app/data-store/services/data-store.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { db } from '../../core/utils/db';
 import { Location } from '../../core/types/location/location';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class DataStoreService {
   private readonly _data: Location[];
+  locationList$: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
 
   constructor() {
     const data = db().read();
     this._data = data != null ? [...data] : [];
+    this.locationList$.next(this._data);
   }
 
   saveData(location: Location) {
     this._data.push(location);
     db().write(this._data);
+    this.locationList$.next(this._data);
   }
 }
