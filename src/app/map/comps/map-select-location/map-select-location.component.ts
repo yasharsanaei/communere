cbef55e8ac2b7845264c7fe7bdcf4dfc62e8ab36
yasharsanayee <yasharsanayee@gmail.com>
2023-03-05 11:44:33 +0330

    map slect location

--- src/app/map/comps/map-select-location/map-select-location.component.ts
+++ src/app/map/comps/map-select-location/map-select-location.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import * as Leaflet from 'leaflet';
 
@@ -8,18 +8,16 @@ import * as Leaflet from 'leaflet';
   styleUrls: ['./map-select-location.component.css'],
 })
 export class MapSelectLocationComponent {
+  @Output()
+  selectedLocation: EventEmitter<any> = new EventEmitter();
+
   constructor() {
     Leaflet.Icon.Default.mergeOptions({
       iconUrl: 'assets/icons/map-location.png',
       iconRetinaUrl: 'assets/icons/map-location.png',
       shadowUrl: '',
       iconSize: [32, 32],
-      // iconAnchor: [12, 41],
-      // popupAnchor: [1, -34],
-      // tooltipAnchor: [16, -28],
       shadowSize: [0, 0],
-      // iconUrl: 'assets/marker-icon.png',
-      // shadowUrl: 'assets/marker-shadow.png',
     });
   }
 
@@ -35,40 +33,25 @@ export class MapSelectLocationComponent {
     center: { lat: 51.5072, lng: 0.1276 },
   };
 
-  initMarkers() {
-    const initialMarkers = [
-      {
-        position: { lat: 51.5072, lng: 0.1276 },
-      },
-    ];
-    initialMarkers.forEach((markerData, index) => {
-      const marker = this.generateMarker(markerData, index);
-      marker.addTo(this.map).bindPopup(`<b>${markerData.position.lat},  ${markerData.position.lng}</b>`);
-      this.map.panTo(markerData.position);
-      this.markers.push(marker);
-    });
-  }
-
   generateMarker(data: any, index: number) {
-    return Leaflet.marker(data.position, { draggable: data.draggable })
-      .on('click', event => this.markerClicked(event, index))
-      .on('dragend', event => this.markerDragEnd(event, index));
+    return Leaflet.marker(data.position).on('click', event => this.markerClicked(event, index));
   }
 
   onMapReady($event: Leaflet.Map) {
     this.map = $event;
-    this.initMarkers();
   }
 
   mapClicked($event: any) {
-    console.log($event.latlng.lat, $event.latlng.lng);
+    const markerData = {
+      position: { lat: $event.latlng.lat, lng: $event.latlng.lng },
+    };
+    const marker = this.generateMarker(markerData, 0);
+    marker.addTo(this.map).bindPopup(`<b>${markerData.position.lat},  ${markerData.position.lng}</b>`);
+    // this.map.panTo(markerData.position);
+    this.markers.push(marker);
   }
 
   markerClicked($event: any, index: number) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
-
-  markerDragEnd($event: any, index: number) {
-    console.log($event.target.getLatLng());
+    console.log($event);
   }
 }
